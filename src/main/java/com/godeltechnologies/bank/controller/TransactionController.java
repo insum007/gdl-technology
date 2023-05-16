57c7b87f75a8c9d2a9832e3f55249c6a311c4f0a
Mohamed Musni <musni@holoteq.com>
2023-05-16 19:19:05 +0530

    added code review changes

--- src/main/java/com/godeltechnologies/bank/controller/TransactionController.java
+++ src/main/java/com/godeltechnologies/bank/controller/TransactionController.java
@@ -2,10 +2,10 @@ package com.godeltechnologies.bank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.godeltechnologies.bank.dto.TransactionRequestDTO;
@@ -36,12 +36,13 @@ public class TransactionController {
 	 * @return ResponseEntity
 	 * 
 	 */
+	@ResponseStatus(value = HttpStatus.OK)
 	@PostMapping("/transaction")
-	public ResponseEntity<?> saveTranaction(@RequestBody TransactionRequestDTO request) throws BusinessException {
+	public TransactionEntity saveTranaction(@RequestBody TransactionRequestDTO request) throws BusinessException {
 		if (request.getCurrency() != null && !request.getCurrency().isBlank() && request.getAmount() != null
 				&& !request.getAmount().equals("") && request.getAccountId() != null && !request.equals(" ")) {
 			log.info("Inside saveTranaction method of TransactionController");
-			return new ResponseEntity<TransactionEntity>(transactionService.saveTranaction(request), HttpStatus.OK);
+			return  transactionService.saveTranaction(request);
 
 		} else {
 			throw new BusinessException(HttpStatus.BAD_REQUEST, "Please provide the required fields.");
